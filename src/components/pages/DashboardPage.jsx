8966d793568b544df8d356b780a05c7a79da72fb
Braden Barnes <bbarnes4318@gmail.com>
2025-09-15 16:43:39 -0400

    Fix dashboard layout - correctly swap Activity Feed and Project Workflow positions

--- src/components/pages/DashboardPage.jsx
+++ src/components/pages/DashboardPage.jsx
@@ -3003,7 +3003,19 @@ const DashboardPage = ({ tasks, activities, onProjectSelect, onAddActivity, colo
           </div>
         </div>
 
-        {/* Left Column - Activity Feed */}
+        {/* Left Column - Project Workflow Line Items Section */}
+        <ProjectWorkflowLineItemsSection
+          projects={projects}
+          colorMode={colorMode}
+          onProjectSelect={onProjectSelect}
+          workflowAlerts={workflowAlerts}
+          alertsLoading={alertsLoading}
+          availableUsers={availableUsers}
+          currentUser={currentUser}
+          handleProjectSelectWithScroll={handleProjectSelectWithScroll}
+        />
+
+        {/* Right Column - Activity Feed */}
         <div className="w-full" data-section="activity-feed">
           <ActivityFeedSection
             activityFeedItems={activityFeedItems}
@@ -3026,18 +3038,6 @@ const DashboardPage = ({ tasks, activities, onProjectSelect, onAddActivity, colo
             handleAddComment={handleAddComment}
           />
         </div>
-
-        {/* Right Column - Project Workflow Line Items Section */}
-        <ProjectWorkflowLineItemsSection
-          projects={projects}
-          colorMode={colorMode}
-          onProjectSelect={onProjectSelect}
-          workflowAlerts={workflowAlerts}
-          alertsLoading={alertsLoading}
-          availableUsers={availableUsers}
-          currentUser={currentUser}
-          handleProjectSelectWithScroll={handleProjectSelectWithScroll}
-        />
 
       {/* Project Cubes - Quick Access (hidden) */}
       <div className="hidden" data-section="project-cubes" aria-hidden="true"></div>
